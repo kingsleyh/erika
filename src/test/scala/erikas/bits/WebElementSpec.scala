98546edf3a6ff9a77346b1d0626841703683a4e9
Kingsley Hendrickse <kingsleyhendrickse@me.com>
2016-04-17 15:10:46 +0100

    updates

--- src/test/scala/erikas/bits/WebElementSpec.scala
+++ src/test/scala/erikas/bits/WebElementSpec.scala
@@ -10,27 +10,32 @@ class WebElementSpec extends FreeSpec with Matchers {
     "getText should return some text" in {
       val element = WebElementHelper()
       element.withGetResponseAction(WebElementResponses.getTextResponse, () => element.get().getText) should be(Some("some text"))
+      element.testDriver.getRequestUrl should be("/session/test-session-id/element/:wdc:1460015822532/text")
     }
 
     "getAttribute should return the attribute" in {
       val element = WebElementHelper()
       element.withGetResponseAction(WebElementResponses.getAttributeResponse, () => element.get()
         .getAttribute("href")) should be(Some("some attribute"))
+      element.testDriver.getRequestUrl should be("/session/test-session-id/element/:wdc:1460015822532/attribute/href")
     }
 
     "click should perform a click" in {
       val element = WebElementHelper()
       element.withPostResponse(anyResponse, () => element.get().click()).getPostRequest should be("""{"id":":wdc:1460015822532"}""")
+      element.testDriver.getRequestUrl should be("/session/test-session-id/element/:wdc:1460015822532/click")
     }
 
     "clear should perform a clear" in {
       val element = WebElementHelper()
       element.withPostResponse(anyResponse, () => element.get().clear()).getPostRequest should be("""{"id":":wdc:1460015822532","sessionId":"test-session-id"}""")
+      element.testDriver.getRequestUrl should be("/session/test-session-id/element/:wdc:1460015822532/clear")
     }
 
     "isEnabled should return boolean" in {
       val element = WebElementHelper()
       element.withGetResponseAction(WebElementResponses.isEnabledResponse, () => element.get().isEnabled) should be(true)
+      element.testDriver.getRequestUrl should be("/session/test-session-id/element/:wdc:1460015822532/enabled")
     }
 
 
