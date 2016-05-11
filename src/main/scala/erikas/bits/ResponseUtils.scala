c19f73ae3decdd8aaef6c2ab8018d8f4053b05d1
hendrkin <kingsley.xa.hendrickse@barclayscapital.com>
2016-05-11 15:09:13 +0100

    update

--- src/main/scala/erikas/bits/ResponseUtils.scala
+++ src/main/scala/erikas/bits/ResponseUtils.scala
@@ -4,14 +4,16 @@ import io.shaka.http.Response
 import argonaut.Argonaut._
 import argonaut.DecodeJson
 
+import scalaz.{\/-, -\/}
+
 
 object ResponseUtils {
 
   implicit class ResponseDecoder(val response: Response) {
     def decode[X: DecodeJson]: X = {
       response.entityAsString.decodeEither[X] match {
-        case Left(message)          => throw APIResponseError(s"\n\nThe response could not be decoded due to\n: $message - \nwith response code: ${response.status.code} \nand message: ${response.entityAsString}\n\n")
-        case Right(decodedResponse) => decodedResponse
+        case -\/(message)          => throw APIResponseError(s"\n\nThe response could not be decoded due to\n: $message - \nwith response code: ${response.status.code} \nand message: ${response.entityAsString}\n\n")
+        case \/-(decodedResponse) => decodedResponse
       }
     }
   }
