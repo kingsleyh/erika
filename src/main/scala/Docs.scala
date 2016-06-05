18fe0ffa9711b98f6734ee6e92a0a5ed01f080d7
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2016-06-05 09:33:30 +0100

    more docs

--- src/main/scala/Docs.scala
+++ src/main/scala/Docs.scala
@@ -3,10 +3,35 @@ import net.kenro.ji.jin.docs._
 object Docs extends App {
 
   val cards = List(
-    Card("setGlobalTimeout", "Sets the timeout used in all the functions that perform an internal timeout while waiting for something", "Unit",
-      List(Param("Int","timeout","(in millis)",ParamType.INPUT)),
-      List(Example("")),
+
+    Card("setGlobalTimeout", "Sets the timeout used in all the functions that perform an internal timeout while waiting for something", "Session",
+      List(Param("Int","timeout","(in millis, defaults to 5000)",ParamType.INPUT)),
+      List(Example("session.setGlobalTimeout(8000)")),
       List(Link("getGlobalTimeout","#getGlobalTimeout")),
+      "src/main/scala/net/kenro/ji/jin/Session.scala"
+    ),
+
+    Card("getGlobalTimeout", "Gets the current value in millis of the global timeout", "Session",
+      List(Param("Int","","",ParamType.OUTPUT)),
+      List(Example("val timeout: Int = session.getGlobalTimeout")),
+      List(Link("setGlobalTimeout", "#setGlobalTimeout")),
+      "src/main/scala/net/kenro/ji/jin/Session.scala"
+    ),
+
+    Card("setAllTimeouts", "Sets all the timeouts to the supplied value in millis", "Session",
+      List(Param("Int","timeout","(in millis)",ParamType.INPUT)),
+      List(Example("session.setAllTimeouts(8000)")),
+      List.empty,
+      "src/main/scala/net/kenro/ji/jin/Session.scala"
+    ),
+
+    Card("visitUrl", "navigate to the supplied url", "Session",
+      List(
+        Param("String", "url", "", ParamType.INPUT),
+        Param("Session", "", "", ParamType.OUTPUT)
+      ),
+      List(Example("session.visitUrl(\"www.google.com\")")),
+      List.empty,
       "src/main/scala/net/kenro/ji/jin/Session.scala"
     )
   )
